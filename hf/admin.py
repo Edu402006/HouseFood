7259b2796996cdb32c5d3607a2ea0a778f9a64fe
Eduardo <edu.rios36@gmail.com>
2022-08-27 09:51:57 -0400

    tercer commit

--- hf/admin.py
+++ hf/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Marca, Producto
+from .models import Marca, Producto, Mesas
 
 # Register your models here.
 
 admin.site.register(Marca)
 admin.site.register(Producto)
+admin.site.register(Mesas)
